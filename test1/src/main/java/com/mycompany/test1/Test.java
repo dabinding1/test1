5c3ff681244ce3c46c5c7eb079946ffd60f1f214
DD <DD@10.2.128.11>
2020-09-08 22:32:00 -0500

    setup

--- test1/src/main/java/com/mycompany/test1/Test.java
+++ test1/src/main/java/com/mycompany/test1/Test.java
@@ -4,11 +4,37 @@
  * and open the template in the editor.
  */
 package com.mycompany.test1;
-
+import java.text.DecimalFormat;
+import java.util.*;
 /**
  *
  * @author DD
  */
 public class Test {
-    
+    public static void main(String[] args){
+        /*Scanner scan = new Scanner(System.in);
+        System.out.println("Hello world");
+        System.out.print("hello ");
+        System.out.flush();
+        System.out.print("hello2");
+        //String next=scan.nextLine();
+        //System.out.println("your input is "+next);
+        
+        String.format("%f", 16.1);
+        */
+        DecimalFormat myFormatter = new DecimalFormat("###,###");
+       // System.out.println(myFormatter.format(11000));
+        StringBuilder myStr = new StringBuilder();
+        for(int i=1;i<7;i++){
+            myStr.append(String.format("%-12d", i));
+        }
+        System.out.println(myStr);
+        myStr=new StringBuilder();
+        for(int i=1;i<7;i++)
+            myStr.append(String.format("%s %2d%-2s",myFormatter.format(10001),16,"%"));
+        System.out.println(myStr);
+        
+        
+  
+    }
 }
